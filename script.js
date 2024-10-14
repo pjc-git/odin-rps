2047720ceb771faa08bc9f1970b2445d79a8fbb9
pjc-git <127760650+pjc-git@users.noreply.github.com>
2024-10-14 12:49:09 +0200

    Create logic for the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("hello!");
+
+function getComputerChoice(){
+    let numberPick = Math.floor(Math.random() * 3);
+    let handPick;
+    if (numberPick == 0) {
+        handPick = "rock";
+    }  else if (numberPick == 1) {
+        handPick = "paper";
+    } else {
+        handPick = "scissors";
+    }
+    return handPick;
+}
+
+console.log(getComputerChoice());
